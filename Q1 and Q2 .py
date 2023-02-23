3728690ed608bc718a9d06fb125cdd429a6fe571
rachelclareparker <125895510+rachelclareparker@users.noreply.github.com>
2023-02-23 17:22:44 +0000

    Add files via upload

---
+++ Q1 and Q2 .py
@@ -0,0 +1,120 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[48]:
+
+
+def PiecewisePoly(Ndeg, x0, y0, x_j):
+    
+    MM = Ndeg + 1                      # degree + 1 = number of points to find
+    N_interpolant = len(x0) - Ndeg      # number of polynomials to get 
+    pts = np.arange(Ndeg+1) + np.arange(N_interpolant).reshape(N_interpolant,1)  # small set of x points
+    
+    a = np.zeros((Ndeg+1,N_interpolant))
+    pows = (MM-1-np.arange(MM)).reshape(MM,1)         
+    
+    # coefs for every interpolant
+    for i in range(N_interpolant):
+        A = np.vander(x0[pts[i,:]])
+        a[:,i] = np.linalg.solve(A,y0[pts[i,:]])     #vander + solving for small set of x points
+    
+     
+    y = np.empty_like(x_j)
+    
+   
+    pows = Ndeg-np.arange(MM)    #putting powers in correct order
+    
+    
+    for n in range(len(x_j)):
+        if((x_j[n]<x0).all()):
+            k=0
+        elif((x_j[n]>x0).all()):
+            k=len(x0)-1
+        else:                # find k for x_i, accounting for the possibility that x_i=x_k
+            k = np.where(((x_j[n]<x0[1:]) & (x_j[n]>=x0[:-1])) | 
+                         ((x0[1:]==x_j[n]) & (x_j[n]>x0[:-1])))[0][0]
+     
+        j = k - Ndeg//2    
+        
+        j = max(0,j)
+        j = min(j,N_interpolant-1)
+        
+        y[n] = np.sum(a[:,j]*x_j[n]**pows)  # obtain new data point
+      
+    return y
+
+
+# In[124]:
+
+
+#testing function using given function and plotting graph over chosen appropriate points
+
+import numpy as np
+
+def f(x):
+    return np.exp(x)
+
+N = 20
+
+
+r0 = np.linspace(0,1/6,N)
+
+x1 = 1/(1- r0) # coordinate transformation to test uneven spacing
+
+y1 = f(x1)
+
+x2 = np.linspace(0.7, 1.5, 100)
+
+Y = PiecewisePoly(10, x1, y1, x2)
+
+import matplotlib.pyplot as plt
+
+get_ipython().run_line_magic('matplotlib', 'inline')
+
+plt.plot(x2, Y, "x", color = "orange")
+plt.plot(x1, y1, "x", color = "black")
+plt.plot(x2, f(x2), color = "purple")
+
+
+# In[139]:
+
+
+def error_function(N, f, a, b, deg):
+    
+    x1 = np.linspace(a, b, N)
+    y1 = f(x1)
+    
+    h = (b-a)/(N-1)
+    
+    evalpts = np.linspace(a, b, 5)
+    
+
+    Y = PiecewisePoly(deg, x1, y1, evalpts)
+    
+    max_error = max(abs(f(evalpts) - Y))
+    
+    return max_error
+
+X = []
+Y = [] 
+
+def plot(deg):
+    
+    for N in range(20, 40, 1):
+    
+        a = 0
+        b = 2
+    
+        h = (b-a)/(N-1)
+    
+        X.append(h)
+        Y.append(error_function(N, f, a, b, deg))
+    
+    plt.plot(X, Y)
+    plt.xlabel("h")
+    plt.ylabel("error")
+    plt.title(N);
+    
+for i in range(3, 6):
+    plot(i)
+
